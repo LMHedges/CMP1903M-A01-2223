5b38ad76f80eb6029f66099bae70c36f33823d2c
Louis Hedges <hedges.louis0@gmail.com>
2023-02-25 18:43:38 +0000

    Push 1

--- Pack.cs
+++ Pack.cs
@@ -13,12 +13,30 @@ namespace CMP1903M_A01_2223
         public Pack()
         {
             //Initialise the card pack here
+            pack = new List<Card>();
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j < 14; j++)
+                {
+                    new Card { Suit = i, Value = j}
+                }
+            }
         }
 
         public static bool shuffleCardPack(int typeOfShuffle)
         {
             //Shuffles the pack based on the type of shuffle
+            switch (typeOfShuffle)
+            {
+                case 1:
+                    Console.WriteLine("Fisher-Yates shuffle");
 
+                case 2:
+                    Console.WriteLine("Riffle shuffle");
+
+                case 3:
+                    Console.WriteLine("No shuffle");
+            }
         }
         public static Card deal()
         {
---
+++ Testing.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Testing
+{
+	public Testing()
+	{
+		
+	}
+}
