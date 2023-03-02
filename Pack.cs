976faa493f406380f2d851dd11580ab016264568
Louis Hedges <hedges.louis0@gmail.com>
2023-03-02 17:18:48 +0000

    Push 6

--- Pack.cs
+++ Pack.cs
@@ -12,7 +12,7 @@ namespace CMP1903M_A01_2223
 
         public Pack()
         {
-            //Initialise the card pack here
+            //Creates the pack
             pack = new List<Card>();
             for (int i = 1; i < 5; i++)
             {
@@ -30,14 +30,23 @@ namespace CMP1903M_A01_2223
             {
                 case 1:
                     Console.WriteLine("Fisher-Yates shuffle");
-
+                    Random R = new Random();
+                    int k;
+                    Card temp;
+                    for(int i = pack.Count - 1; i > 0; i--)
+                    {
+                        k = R.Next(i + 1);
+                        temp = pack[i];
+                        pack[i] = pack[k];
+                        pack[k] = temp;
+                    }
                     return true;
                 case 2:
                     Console.WriteLine("Riffle shuffle");
                     Card[] pile1 = new Card[26];
                     Card[] pile2 = new Card[26];
                     List<Card> temp_pack = new List<Card>();
-                    for (int i = 0; i < 26; i++)
+                    for (int i = 0; i < 26; i++) //splits pack in two
                     {
                         pile1[i] = pack[i];
                         pile2[i] = pack[i+26];
@@ -65,8 +74,9 @@ namespace CMP1903M_A01_2223
         public static List<Card> dealCard(int amount)
         {
             //Deals the number of cards specified by 'amount'
-            List<Card> delt = (List<Card>)pack.Take(amount);
-            return delt;
+            IEnumerable<Card> enumerable = pack.Take(amount);
+            List<Card> cards = enumerable.ToList();
+            return cards;
         }
     }
 }
--- Testing.cs
+++ Testing.cs
@@ -8,11 +8,129 @@ namespace CMP1903M_A01_2223
 {
     class Testing
     {
+        private static string card_type(Card card)
+        {
+            string suit = string.Empty;
+            string value;
+            string card_name;
+            switch (card.Suit)
+            {
+                case 1:
+                    suit = "Hearts";
+                    break;
+                case 2:
+                    suit = "Clubs";
+                    break;
+                case 3:
+                    suit = "Dimonds";
+                    break;
+                case 4:
+                    suit = "Spades";
+                    break;
+            }
+            switch (card.Value)
+            {
+                case 1:
+                    value = "Ace";
+                    break;
+                case 11:
+                    value = "Jack";
+                    break;
+                case 12:
+                    value = "Queen";
+                    break;
+                case 13:
+                    value = "King";
+                    break;
+                default:
+                    value = (card.Value).ToString();
+                    break;
+            }
+            card_name = value + " of " + suit;
+            return card_name;
+        }
+        private static bool shuffle()
+        {
+            int input;
+            Console.WriteLine("1. Fisher-Yates Shuffle");
+            Console.WriteLine("2. Riffle Shuffle");
+            Console.WriteLine("3. No Shuffle");
+            try
+            {
+                input = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                return false; //if user doesn't enter an integer
+            }
+            return Pack.shuffleCardPack(input); //Returns true if valid input and shuffle completed
+        }
+        private static bool deal()
+        {
+            int input;
+            Console.WriteLine("How many cards do you wont delt?");
+            try
+            {
+                input = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                return false; //if user doesn't enter an integer
+            }
+            if(input == 1)
+            {
+                Card card = Pack.deal();
+                Console.WriteLine(card_type(card));
+            }
+            else
+            {
+                if(input < 2 || input > 52)
+                {
+                    return false; //if user enters an invalid integer
+                }
+                List<Card> cards = Pack.dealCard(input);
+                foreach(Card card in cards)
+                {
+                    Console.WriteLine(card_type(card));
+                }
+            }
+            return true;
+        }
         public static void test()
         {
-            Pack test1 = new Pack();
-            bool test = Pack.shuffleCardPack(2);
-            Console.WriteLine(test);
+            new Pack();
+            bool valid = true;
+            bool valid_shuffle;
+            bool valid_deal;
+            string input;
+            while (valid)
+            {
+                Console.WriteLine("1. Shuffle");
+                Console.WriteLine("2. Deal");
+                Console.WriteLine("3. Quit");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        valid_shuffle = shuffle();
+                        if(valid_shuffle == false)
+                        {
+                            Console.WriteLine("Not valid input, please try again");
+                        }
+                        break;
+                    case "2":
+                        valid_deal = deal();
+                        if(valid_deal == false)
+                        {
+                            Console.WriteLine("Not valid input, please try again");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Goodbye");
+                        valid = false;
+                        break;
+                }
+            }           
         }
     }
 }
